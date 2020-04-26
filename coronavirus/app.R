72dbe11e6af88df5c539e769771a1469ef39c606
Jessica Wu <jessicawu@Jessicas-MacBook-Pro-3.local>
2020-04-25 23:08:09 -0400

    loaded google search data

--- coronavirus/app.R
+++ coronavirus/app.R
@@ -15,26 +15,29 @@ ui <- navbarPage(
                        titlePanel("Cases of Coronavirus Over Time"),
                        mainPanel(plotOutput("line_plot")))
     ),
-    tabPanel("Death",
-             titlePanel("Death in the Time of Coronavirus"),
-             p("Tour of the modeling choices you made and 
-              an explanation of why you made them")),
     tabPanel("Love",
              titlePanel("Love in the Time of Coronavirus"),
              p("Tour of the modeling choices you made and 
               an explanation of why you made them")),
-    tabPanel("Economy",
-             titlePanel("Economy in the Time of Coronavirus"),
+    tabPanel("Sorrow",
+             titlePanel("Sorrow in the Time of Coronavirus"),
              p("Tour of the modeling choices you made and 
               an explanation of why you made them")),
     tabPanel("About", 
              titlePanel("About"),
              h3("Project Background and Motivations"),
-             p("Hello, this is where I talk about my project."),
+             p("'In the Time of Coronavirus' seeks to explore the untold stories
+             of coronavirus from the lenses of life, death, love, and economy.
+             We are being inundated with news about the number of coronavirus
+             cases and key figures that speak to our physical health, but there
+             are endless stories to be told about the impact of coronavirus on 
+             our daily lives and the rest of the human existence -- from the 
+             deteriation of our relationships to struggling to put food on the
+             table."),
              h3("About Me"),
-             p("My name is Jessica, and I study Applied Math and Economics. 
+             p("My name is Jessica, and I study Applied Math and Economics.
              You can reach me at jessicawu@college.harvard.edu."))
-)
+             )
 
 
 server <- function(input, output) {
