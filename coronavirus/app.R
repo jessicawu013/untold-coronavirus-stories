1102310240426a0f894aadd53039fc8171566f6e
Jessica Wu <jessicawu@Jessicas-MacBook-Pro-3.local>
2020-04-27 22:23:23 -0400

    updated app with text

--- coronavirus/app.R
+++ coronavirus/app.R
@@ -1,3 +1,4 @@
+# Load packages
 
 library(shiny)
 library(shinythemes)
@@ -5,6 +6,8 @@ library(knitr)
 library(scales)
 library(ggthemes)
 library(lubridate)
+library(choroplethr)
+library(choroplethrMaps)
 library(tidyverse)
 
 # loading data
@@ -73,6 +76,71 @@ long_dist <- read_csv("data-sources/longdistance.csv",
     tail(-44) %>%
     mutate(long_dist = long_dist / 42 * 100)
 
+divorce <- read_csv("data-sources/divorce.csv",
+                    skip = 1,
+                    col_types = cols(
+                        Day = col_date(format = ""),
+                        `Divorce: (United States)` = col_double()
+                    )) %>%
+    rename(divorce = `Divorce: (United States)`) %>%
+    mutate(divorce = divorce / 82 * 100)
+
+divorce_lawyer <- read_csv("data-sources/divorcelawyer.csv",
+                           skip = 1,
+                           col_types = cols(
+                               Day = col_date(format = ""),
+                               `divorce lawyer: (United States)` = col_double()
+                           )) %>%
+    rename(divorce_lawyer = `divorce lawyer: (United States)`) %>%
+    mutate(divorce_lawyer = divorce_lawyer / 79 * 100)
+
+legal_sep <- read_csv("data-sources/legalseparation.csv",
+                      skip = 1,
+                      col_types = cols(
+                          Day = col_date(format = ""),
+                          `Legal separation: (United States)` = col_double()
+                      )) %>%
+    rename(legal_sep = `Legal separation: (United States)`) %>%
+    mutate(legal_sep = legal_sep / 56 * 100)
+
+divorce_terms <- divorce %>%
+    left_join(divorce_lawyer, by = "Day") %>%
+    left_join(legal_sep, by = "Day") %>%
+    tail(-44) %>%
+    rename(date = Day) %>%
+    pivot_longer(-date, names_to = "search", values_to = "frequency")
+
+dom_violence <- read_csv("data-sources/domesticviolence.csv",
+                         skip = 1,
+                         col_types = cols(
+                             Day = col_date(format = ""),
+                             `domestic violence: (United States)` = col_double()
+                         )) %>%
+    rename(dom_violence = `domestic violence: (United States)`) %>%
+    tail(-44) %>%
+    mutate(dom_violence = dom_violence / 78 * 100)
+
+# Data for Sorrow
+
+covid <- read_csv("data-sources/us-counties-covid-04-26.csv",
+                  col_types = cols(
+                      date = col_date(format = ""),
+                      county = col_character(),
+                      state = col_character(),
+                      fips = col_double(),
+                      cases = col_double(),
+                      deaths = col_double()
+                  )) %>%
+    filter(date == "2020-04-25")
+
+data(df_pop_county) 
+data("df_county_demographics")
+df_county_demographics$value = df_county_demographics$percent_black
+
+chloropleth_covid <- df_pop_county %>%
+    left_join(covid, by = c("region" = "fips"))
+
+chloropleth_covid$value = chloropleth_covid$cases
 
 # ui
 ui <- navbarPage(
@@ -88,28 +156,60 @@ ui <- navbarPage(
     ),
     tabPanel("Love",
              fluidPage(titlePanel("Love in the Time of Coronavirus"),
-             p("How is the face of love changing during quarantine?"),
-             mainPanel(plotOutput("long_dist_plot")))
-             ),
+             HTML("<strong>How is the face of love changing during quarantine?</strong>"),
+             mainPanel(h4("Long Distance"),
+                       p("People must suddenly be finding themselves forced into 
+               long-distance relationships. Do Google Search trends support
+               this conclusion?"),
+                       plotOutput("long_dist_plot")),
+             mainPanel(h4("Divorce"),
+                       p("On the other hand, people must have been suddenly forced to spend
+                         essentially 24/7 with their partners. Is it possible that
+                         too much time together exacerbates marital problems,
+                         perhaps even bringing them to a head?"),
+                       plotOutput("divorce_plot")),
+             mainPanel(h4("Domestic Violence*"),
+                       p("Many experts, including the", a(href= "https://www.apa.org/topics/covid-19/domestic-violence-child-abuse",
+                         "American Psychological Association,"),"have suggested
+                         that people are facing increased domestic violence 
+                         under stay-at-home orders. I looked to Google Search
+                         trends to see if this was possibly captured by web activity."),
+                       plotOutput("dom_violence_plot"),
+                       p("*Although under this category, domestic violence is
+                         NOT love; it is abuse."))
+             )),
     tabPanel("Sorrow",
              titlePanel("Sorrow in the Time of Coronavirus"),
-             p("Tour of the modeling choices you made and 
-              an explanation of why you made them")
+             p("The sad reality of our times is that there is a lot of sorrow."),
+             mainPanel(plotOutput("us_black_map"),
+                       plotOutput("us_cases_map"),
+                       plotOutput("ny_black_map"),
+                       plotOutput("ny_cases_map"))
              ),
     tabPanel("About", 
              titlePanel("About"),
-             h3("Project Background and Motivations"),
-             p("'In the Time of Coronavirus' seeks to explore the untold stories
-             of coronavirus from the lenses of life, death, love, and economy.
+             h3("About the Project"),
+             p("I first began thinking about focusing on COVID-19 for my final
+             project back in mid-February 2020. At the time, very few people in
+             the United States had any idea just how intensely and quickly this
+             pandemic would come to dominate our lives, myself included. In a mere 
+             matter of weeks, the world's top data scientists began focusing on
+             documenting the spread of the novel coronavirus. I wondered to myself,
+             'What narratives are still important but being left out of the main news
+             cycle? When we look back in 50 years at the history that is being made,
+             what stories will we forget to tell?'"),
+             HTML('<strong> "In the Time of Coronavirus" </strong>, a play on the famous
+             novel and film, "Love in the Time of Cholera," seeks to explore 
+             the untold stories of coronavirus from the lenses of life, love, and sorrow.
              We are being inundated with news about the number of coronavirus
              cases and key figures that speak to our physical health, but there
              are endless stories to be told about the impact of coronavirus on 
              our daily lives and the rest of the human existence -- from the 
              deterioration of our relationships to struggling to put food on the
-             table."),
-             h3("About Me"),
-             p("My name is Jessica, and I study Applied Math and Economics.
-             You can reach me at jessicawu@college.harvard.edu."))
+             table.'),
+             h3("About Jessica"),
+             p("I am a first-year at Harvard College studying Applied
+             Math and Economics. You can reach me at jessicawu@college.harvard.edu."))
              )
 
 
@@ -152,6 +252,73 @@ server <- function(input, output) {
                       hjust = -0.20,
                       size = 3) +
             theme_calc()
+    )
+    
+    output$divorce_plot <- renderPlot(
+        divorce_terms %>%
+            ggplot() +
+            geom_line(aes(x = date, y = frequency, color = search)) + 
+            geom_vline(xintercept=as.numeric(divorce_terms$date[c(18, 19, 37)]),
+                       linetype=2, colour="black") +
+            theme_calc() +
+            labs(title = "How are people feeling about their marriages?",
+                 subtitle = "Taking a look a Google Search trends",
+                 y = "Percent (%) Frequency of Search \n\ Relative to Itself on March 10th",
+                 x = "Date") +
+            scale_color_discrete(name = "Searches", labels = c('Topic: Divorce',
+                                                               'Term: "divorce lawyer"',
+                                                               'Topic: Legal separation')) +
+            geom_text(data = events, mapping = aes(label = text, y = 0, x = time), 
+                      angle = 90,
+                      hjust = -0.20,
+                      size = 3)
+    )
+    
+    output$dom_violence_plot <- renderPlot(
+        ggplot(dom_violence, aes(x = Day, y = dom_violence)) +
+            geom_line(color = "red") +
+            labs(
+                title = "Interest in Domestic Violence During Social Distancing",
+                subtitle = "Taking a look at Google Search Trends",
+                y = "Popularity of the Topic, 'Domestic Violence', \n\ Relative to March 10th (%)",
+                x = "Date"
+            ) +
+            geom_vline(xintercept=as.numeric(long_dist$Day[c(6, 7, 13)]),
+                       linetype=2, colour="black") +
+            geom_text(data = events, mapping = aes(label = text, y = 50, x = time), 
+                      angle = 90,
+                      hjust = -0.20,
+                      size = 3) +
+            theme_calc()
+    )
+    
+    output$us_black_map <- renderPlot(
+        
+        county_choropleth(df_county_demographics,
+                          title = "Demographics of the United States in 2013\nPercent Black",
+                          legend = "Percent Black")
+    )
+    
+    output$us_cases_map <- renderPlot(
+        county_choropleth(chloropleth_covid,
+                          num_colors = 9,
+                          title = "Number of Coronavirus Cases as of April 25th, 2020", 
+                          legend = "Number of Cases")
+    )
+    
+    output$ny_black_map <- renderPlot(
+        county_choropleth(df_county_demographics,
+                          title = "Demographics of New York in 2013\nPercent Black", 
+                          legend = "Percent Black",
+                          state_zoom = "new york")
+    )
+    
+    output$ny_cases_map <- renderPlot(
+        county_choropleth(chloropleth_covid,
+                          num_colors = 9,
+                          title = "Coronavirus Cases in New York as of April 25th, 2020", 
+                          legend = "Number of Cases",
+                          state_zoom = "new york")
     )
 }
 
